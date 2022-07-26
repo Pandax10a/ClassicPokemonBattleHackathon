cf25dab0f0b8e005af830d2ea911af0d6591eab4
panda1 <blueblue0011@gmail.com>
2022-07-25 20:26:15 -0700

    added attack but need to make it cumulative

--- battle.js
+++ battle.js
@@ -1,4 +1,5 @@
-
+/* a limited pokedex for pokemon with data being used from the game itself organized in object. enemies can be made
+into an array in later updates to randomize encounters*/
 let enemies = {
     name: `muk`,
     image_url: `https://projectpokemon.org/images/shiny-sprite/muk.gif`,
@@ -27,17 +28,27 @@ let blastoise = {
     Att: [`Hydro Pump`, `Skull Bash`, `Hydro Cannon`],  
 }
 
+Cookies.set(`my_hp`);
+Cookies.set(`computer_hp`);
+
 // let muk_json = muk.stringify(muk);
 // Cookies.set(`enemy_chosen`, muk_json);
 // Cookies.set(`enemy_chosen`);
 // Cookies.set(`current_user`);
 let player_1_info = Cookies.get(`player_1`);
 
+/* for each if conditional, it matches the selected from index page, to match the data held in battle.js. 
+it populates the battle page with enemy pokemon setup, the player selected pokemon, and the basic attacks using called
+functions, it also sets up the initial value for hp to cookies for both the computer and player   */
 if (player_1_info === `raticate`) {
     let raticate_json = JSON.stringify(raticate);
     Cookies.set(`current_user`, raticate_json);
     let enemies_json = JSON.stringify(enemies);
     Cookies.set(`enemy_chosen`, enemies_json);
+    let starting_hp = raticate[`HP`];
+    Cookies.set(`my_hp`, `${starting_hp}`);
+    let computer_starting_hp = enemies[`HP`];
+    Cookies.set(`computer_hp`, computer_starting_hp);
     window.addEventListener(`load`, selected_enemy);
     window.addEventListener(`load`, selected_player(raticate));
     window.addEventListener(`load`, set_button_att(raticate));
@@ -46,6 +57,10 @@ if (player_1_info === `raticate`) {
     Cookies.set(`current_user`, magikarp_json);
     let enemies_json = JSON.stringify(enemies);
     Cookies.set(`enemy_chosen`, enemies_json);
+    let starting_hp = magikarp[`HP`];
+    Cookies.set(`my_hp`, `${starting_hp}`);
+    let computer_starting_hp = enemies[`HP`];
+    Cookies.set(`computer_hp`, computer_starting_hp);
     window.addEventListener(`load`, selected_enemy);
     window.addEventListener(`load`, selected_player(magikarp));
     window.addEventListener(`load`, set_button_att(magikarp));
@@ -54,6 +69,10 @@ if (player_1_info === `raticate`) {
     Cookies.set(`current_user`, blastoise_json);
     let enemies_json = JSON.stringify(enemies);
     Cookies.set(`enemy_chosen`, enemies_json);
+    let starting_hp = blastoise[`HP`];
+    Cookies.set(`my_hp`, `${starting_hp}`);
+    let computer_starting_hp = enemies[`HP`];
+    Cookies.set(`computer_hp`, computer_starting_hp);
     window.addEventListener(`load`, selected_enemy);
     window.addEventListener(`load`, selected_player(blastoise));
     window.addEventListener(`load`, set_button_att(blastoise));
@@ -99,3 +118,16 @@ player_display.insertAdjacentHTML(`afterbegin`, `<h1>You have spotted a ${enemie
 function set_button_att (a) {
     document.getElementById(`gotta_catch_em_all`).insertAdjacentHTML(`afterbegin`, `<button id = 'attack_1'>${a[`Att`][0]}</button> <button id = 'attack_2'>${a[`Att`][1]}</button> <button id = 'attack_3'>${a[`Att`][2]}</button>`);
 }
+
+let battle_box_1 = document.getElementById(`attack_1`);
+battle_box_1.addEventListener(`click`, quick_attack(11))
+
+function quick_attack (trueDmg) {
+let temp_boss_hp = Cookies.get(`computer_hp`);
+temp_boss_hp -= trueDmg;
+console.log(temp_boss_hp);
+Cookies.set(`computer_hp`, temp_boss_hp);
+}
+
+let battle_log_display = document.querySelector(`#battle_log`)
+battle_log_display.insertAdjacentHTML(`afterbegin`, `<h1>Boss hp: ${Cookies.get(`computer_hp`)}`);
